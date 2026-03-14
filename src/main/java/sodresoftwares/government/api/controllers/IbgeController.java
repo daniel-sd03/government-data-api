42b491540e27db8838a6e246ab778f8cb9ac1fbc
daniel-sd03 <dsodre2003@gmail.com>
2026-03-14 19:47:46 -0300

    refactor(ibge): implement ApiException and improve error handling in service and controller

--- src/main/java/sodresoftwares/government/api/controllers/IbgeController.java
+++ src/main/java/sodresoftwares/government/api/controllers/IbgeController.java
@@ -1,22 +1,34 @@
 package sodresoftwares.government.api.controllers;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import sodresoftwares.government.api.exception.ApiException;
+import sodresoftwares.government.api.model.user.StateDTO;
 import sodresoftwares.government.api.services.IbgeService;
 
 @RestController
 public class IbgeController {
 
 	private final IbgeService ibgeService;
-	
+
 	public IbgeController(IbgeService ibgeService) {
 		this.ibgeService = ibgeService;
 	}
-	
-    @GetMapping("/states")
-    public ResponseEntity<?> getStates() {
-        return ResponseEntity.ok(ibgeService.getStates());
-    }
+
+	@GetMapping("/states")
+	public ResponseEntity<?> getStates() {
+		try {
+			List<StateDTO> states = ibgeService.getStates();
+			return ResponseEntity.ok(states);
+
+		} catch (ApiException e) {
+			return ResponseEntity.status(e.getStatusCode()).body(List.of());
+		} catch (Exception e) {
+			return ResponseEntity.status(500).body(List.of());
+		}
+	}
 }
---
+++ src/main/java/sodresoftwares/government/api/exception/ApiException.java
@@ -0,0 +1,17 @@
+package sodresoftwares.government.api.exception;
+
+public class ApiException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private final int statusCode;
+
+	public ApiException(int statusCode, String message) {
+		super(message);
+		this.statusCode = statusCode;
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+}
