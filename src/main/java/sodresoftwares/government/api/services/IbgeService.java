5fbd3673f73e1ea0282871b846740a8b8bee2e1a
Daniel Sodré <dsodre2003@gmail.com>
2026-03-14 19:49:55 -0300

    refactor(ibge): implement ApiException and improve error handling in service and controller (#4)

--- src/main/java/sodresoftwares/government/api/services/IbgeService.java
+++ src/main/java/sodresoftwares/government/api/services/IbgeService.java
@@ -2,21 +2,47 @@ package sodresoftwares.government.api.services;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import sodresoftwares.government.api.client.IbgeClient;
+import sodresoftwares.government.api.exception.ApiException;
 import sodresoftwares.government.api.model.user.StateDTO;
 
 @Service
 public class IbgeService {
 
+	private static final Logger logger = LoggerFactory.getLogger(IbgeService.class);
+
     private final IbgeClient ibgeClient;
-    
+
     public IbgeService(IbgeClient ibgeClient) {
-    	this.ibgeClient = ibgeClient;
+        this.ibgeClient = ibgeClient;
     }
 
     public List<StateDTO> getStates() {
-        return ibgeClient.getStates();
+        try {
+        	
+        	logger.debug("Calling IBGE API to fetch states");
+            List<StateDTO> states = ibgeClient.getStates();
+            logger.debug("States returned by IBGE API: {}", states.size());
+            
+            if (states == null || states.isEmpty()) {
+                logger.warn("No states found in API do IBGE response ");
+                throw new ApiException(404, "No states found");
+            }
+            
+            return states;
+
+        } catch (WebClientResponseException e) {
+            logger.error("IBGE API error: status={}, body={}", e.getStatusCode().value(), e.getResponseBodyAsString());
+            throw new ApiException(e.getStatusCode().value(), "IBGE API error: " + e.getMessage());
+
+        } catch (Exception e) {
+            logger.error("Failed to connect to IBGE API", e);
+            throw new ApiException(501, "Failed communicate with IBGE API: " + e.getMessage());
+        }
     }
 }
