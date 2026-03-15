7facfb38eb0062996d51c96582a930c397489782
Daniel Sodré <dsodre2003@gmail.com>
2026-03-15 09:26:46 -0300

    refactor(exception): add GlobalExceptionHandler and simplify controller/service (#6)

--- src/main/java/sodresoftwares/government/api/controllers/IbgeController.java
+++ src/main/java/sodresoftwares/government/api/controllers/IbgeController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import sodresoftwares.government.api.exception.ApiException;
 import sodresoftwares.government.api.model.user.StateDTO;
 import sodresoftwares.government.api.services.IbgeService;
 
@@ -18,17 +17,10 @@ public class IbgeController {
 	public IbgeController(IbgeService ibgeService) {
 		this.ibgeService = ibgeService;
 	}
-
+	
 	@GetMapping("/states")
 	public ResponseEntity<?> getStates() {
-		try {
 			List<StateDTO> states = ibgeService.getStates();
 			return ResponseEntity.ok(states);
-
-		} catch (ApiException e) {
-			return ResponseEntity.status(e.getStatusCode()).body(List.of());
-		} catch (Exception e) {
-			return ResponseEntity.status(500).body(List.of());
-		}
 	}
 }
---
+++ src/main/java/sodresoftwares/government/api/model/user/ErrorResponseDTO.java
@@ -0,0 +1,5 @@
+package sodresoftwares.government.api.model.user;
+
+public record ErrorResponseDTO(int status, String message) {
+
+}
